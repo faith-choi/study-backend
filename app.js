bf29ed89f74f870b0dab6cf931c4839ce9aef708
choi <faith-choi@github.com>
2022-08-11 16:38:11 +0900

    ejs 연습

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const app = express();
+app.set("view engine", "ejs");
+
+app.use("/static", express.static("static"));
+// app.use("/static", express.static(__dirname + "/static"));
 
 const port = 8000;
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  let person = [
+    {
+      name: "김소연",
+      gender: "여자",
+    },
+    {
+      name: "최다혜",
+      gender: "여자",
+    },
+  ];
+  res.render("test", { per: person });
+});
+
+app.get("/main", (req, res) => {
+  res.render("main");
 });
 
 app.listen(port, () => {
