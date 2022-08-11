9f6277a280030eebba6df471fe3f5a91fd0d1ba1
choi <faith-choi@github.com>
2022-08-11 17:43:40 +0900

    study: nodejs use ejs

--- app.js
+++ app.js
@@ -8,21 +8,7 @@ app.use("/static", express.static("static"));
 const port = 8000;
 
 app.get("/", (req, res) => {
-  let person = [
-    {
-      name: "김소연",
-      gender: "여자",
-    },
-    {
-      name: "최다혜",
-      gender: "여자",
-    },
-  ];
-  res.render("test", { per: person });
-});
-
-app.get("/main", (req, res) => {
-  res.render("main");
+  res.render("test");
 });
 
 app.listen(port, () => {
